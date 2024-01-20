edb766ae0202ab6d726e6a82fe8216c41edc9a1a
chaeyeonlee07 <lcy2507@naver.com>
2024-01-19 21:03:32 -0800

    daily

--- nwitter-reloaded/src/App.tsx
+++ nwitter-reloaded/src/App.tsx
@@ -1,4 +1,8 @@
-import { useState } from 'react'
+import { createBrowserRouter } from 'react-router-dom';
+
+const router = createBrowserRouter([{
+  path: "/", element: <Layout />, children: [{ path: "", element: <Home /> }, { path: "profile", element: <Profile /> }]
+}])
 
 
 function App() {
